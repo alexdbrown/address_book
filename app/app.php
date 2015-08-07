c34761e137d3c489931d4f9bd4239f5451698622
alexdbrown <student@epicodus.com>
2015-08-07 10:58:36 -0700

    add contact created route on app.php

--- app/app.php
+++ app/app.php
@@ -32,18 +32,18 @@
         return $app['twig']->render('contacts.html.twig', array('contacts' => Contact::getAll()));
     });
 
-    // $app->post("/contacts", function () {
-    //     $contact = new Contact($_POST['name'], $_POST['phone'], $_POST['address']);
-    //     $contact->save();
-    //     return //twig rendering here
-    //
-    // });
-    //
-    // $app->post("/delete_tasks", function () {
-    //
-    //     Task::deleteAll();
-    //
-    // });
+    $app->post("/contacts", function () use ($app) {
+        $contact = new Contact($_POST['name'], $_POST['phone'], $_POST['address']);
+        $contact->save();
+        return $app['twig']->render('create_contact.html.twig');
+
+    });
+
+    $app->post("/delete_tasks", function () {
+
+        Task::deleteAll();
+
+    });
 
     return $app;
 
