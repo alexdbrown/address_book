c059413fbdebecfa2a415222f5835b9db19bf3c2
alexdbrown <student@epicodus.com>
2015-08-07 10:48:26 -0700

    added view folder and implemented twig templating

--- app/app.php
+++ app/app.php
@@ -1,42 +1,49 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../Address_book.php";
+    require_once __DIR__."/../src/Address_book.php";
 
     session_start();
+
     if (empty($_SESSION['list_of_addresses'])) {
         $_SESSION['list_of_addresses'] = array();
     }
 
     $app = new Silex\Application();
 
-    $app->get("/", function() {
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
 
-        $output = "";
+    ));
 
-        $all_contacts = Contact::getAll();
-        //previously instantiated platforms - not static yet
-        $list_of_addresses = array(//need to add instantiated tasks here??)
+    $app->get("/", function() use ($app) {
 
-        foreach (Contct::() as $contact) {
-            $output .= "<p>" . $contact->getContact() . "</p>";
-        }
+    //     $output = "";
+    //
+    //     $all_contacts = Contact::getAll();
+    //
+    //     //previously instantiated platforms - not static yet
+    //     $list_of_addresses = array(//need to add instantiated tasks here??)
+    //
+    //     foreach ($all_contacts as $contact) {
+    //         $output .= "<p>" . $contact->getContact() . "</p>";
+    //     }
+    // }
 
-        return $output;
-
+        return $app['twig']->render('contacts.html.twig', array('contacts' => Contact::getAll()));
     });
 
-    $app->post("/contacts", function () {
-        $contact = new Contact($_POST['name'], $_POST['phone'], $_POST['address']);
-        $contact->save();
-        return //twig rendering here
-
-    });
-
-    $app->post("/delete_tasks", function () {
-
-        Task::deleteAll();
-
-    });
+    // $app->post("/contacts", function () {
+    //     $contact = new Contact($_POST['name'], $_POST['phone'], $_POST['address']);
+    //     $contact->save();
+    //     return //twig rendering here
+    //
+    // });
+    //
+    // $app->post("/delete_tasks", function () {
+    //
+    //     Task::deleteAll();
+    //
+    // });
 
     return $app;
 
